111ca1793a9788045887c0c9dc0715e86e4781f2
ariful360 <arfulislmqa@gmail.com>
2024-05-28 21:20:49 -0400

    homework may

---
+++ src/hw4Constructor/StudentTest.java
@@ -0,0 +1,11 @@
+package hw4Constructor;
+
+public class StudentTest {
+	public static void main(String[] args) {
+		Student student = new Student();
+
+		Student ariful = new Student("Ariful Islam", 285267, 'M', true, 98.67f);
+
+	}
+
+}
